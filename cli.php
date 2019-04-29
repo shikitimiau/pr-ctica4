bbcf41ac2e603896351b5546ba80f678268129e6
angelAlk <angel2000zx@comunidad.unam.mx>
2019-04-28 22:49:56 -0500

    feat: Added file contents

--- cli.php
+++ cli.php
@@ -3,14 +3,26 @@
 <?php
 # php can be used from command line, but I don't know how to toggle it in windows.
 
-function make_me_a_controller(array $args):void{
+function make_me_a_controller(array $args, string $file_content):void{
 	//remember the 1st argument is always the name
 	//lets receive a name for the controller file
 
 	if(isset($args[1])){
-		echo $args[1];
+		echo $args[1].PHP_EOL;
+		echo $file_content;
 	}else
 		echo "Dude I need a name for the file";
 }
+$text = <<<HTML
+<?php
+	include "ejemplo/controllers.php";
 
-make_me_a_controller($argv);
+	//set your custom function here :D
+	class /NAME/ extends Controller{
+		function main():void{}
+		function failure():string{}
+	}
+?>
+HTML;
+
+make_me_a_controller($argv,$text);
